34d886d9c75750966fb64232294aae52d6de1f54
Shirasa P <shirasapraja@gmail.com>
2023-09-26 19:33:50 -0600

    update myapp.js

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let express = require('express');
 
 let app = express();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://sara-raja:GMlyF6PZqsjFFTjX@cluster0.6jakbkf.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // absolutePath = __dirname + '/views/index.html';
 
